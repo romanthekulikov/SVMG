64a1b9614281051ca351b63333ddc8efc4700fe5
Roman Kulikov <gadamob@gmail.com>
2025-11-29 22:24:05 +0300

    Fix minor

--- app/src/main/java/com/roman_kulikov/svmg/MainState.kt
+++ app/src/main/java/com/roman_kulikov/svmg/MainState.kt
@@ -5,7 +5,7 @@ import com.roman_kulikov.processor.annotations.StateField
 import com.roman_kulikov.processor.annotations.UIState
 
 @UIState
-class MainState(
+data class MainState(
     @StateField(FunctionTarget.SET)
     val humanParam: HumanParam = HumanParam("", 0)
 )
